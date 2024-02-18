16c9ad2584633589b3e89aab3112bf52a7732f3c
Administrator <Administrator@DESKTOP-J8KTD2I>
2024-02-18 10:35:25 +0530

    assignment2 feb 17th has relationship btw different project project xyz

---
+++ ProjectXYZ/src/package2/ClassHasRelDifPkg.java
@@ -0,0 +1,14 @@
+package package2;
+
+import package1.ClassStudent;
+
+public class ClassHasRelDifPkg {
+	
+	public static void main(String[] args) {
+		
+		ClassStudent ob=new ClassStudent();
+		ob.method1();
+		ob.method2();
+	}
+
+}
