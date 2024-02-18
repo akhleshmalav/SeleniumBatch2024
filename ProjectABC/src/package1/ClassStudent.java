d8cd09b638c077e00ebc851f761f04a428fc0d67
Administrator <Administrator@DESKTOP-J8KTD2I>
2024-02-18 10:33:15 +0530

    assignment2 feb 17th has relationship btw different project

---
+++ ProjectABC/src/package1/ClassStudent.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class ClassStudent {
+	
+	public void method1() {
+		
+		System.out.println("This is ProjectABC method1");
+	}
+    public void method2() {
+		
+		System.out.println("This is ProjectABC method2");
+	}
+    
+    public static void main(String[] args) {
+		
+	}
+
+}
